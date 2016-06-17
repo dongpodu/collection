b244ee9181c892754d0c5de0b6621ad4f446688e
duyisong <duyisong@172.16.3.101>
2016-06-17 11:17:41 +0800

    shell排序实现

--- src/com/elisonwell/algorithm/TestSort.java
+++ src/com/elisonwell/algorithm/TestSort.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 /**
  * 排序算法比较
+ * 此处实现均是我自己实现的
  * @author duyisong
  * @createAt 2016年6月15日
  */
@@ -54,6 +55,65 @@ public class TestSort {
 		System.out.println("select耗时:"+(t1-t));
 	}
 	
+	/**
+	 * 插入排序
+	 * 数据量   耗时（s）
+	 * 100w    453
+	 * 10w     4
+	 * @param arrays
+	 */
+	public static void insert(int[] arrays){
+		long t = System.currentTimeMillis();
+		int tmp;
+		for(int i=1;i<arrays.length;i++){
+			for(int j=i;j>0;j--){
+				if(arrays[j]<arrays[j-1]){
+					tmp = arrays[j];
+					arrays[j] = arrays[j-1];
+					arrays[j-1] = tmp;
+				}else{
+					break; //a[j]前面的数据是从小到大的，因此当a[j]>=arrays[j-1]时，则无需再向下比较
+				}
+			}
+		}
+		long t1 = System.currentTimeMillis();
+		System.out.println("insert耗时:"+(t1-t));
+	}
+	
+	/**
+	 * 希尔排序
+	 * 希尔排序通过将比较的全部元素分为几个区域来提升插入排序的性能。这样可以让一个元素可以一次性地朝最终位置前进一大步。
+	 * 然后算法再取越来越小的步长进行排序，算法的最后一步就是普通的插入排序，但是到了这步，需排序的数据几乎是已排好的了（此时插入排序较快）。
+	 * 数据量   耗时（s）
+	 * 100w    466
+	 * 10w     4
+	 * @param arrays
+	 */
+	public static void shell(int[] arr) {
+		long t = System.currentTimeMillis();
+		int gap = 1, i, j, len = arr.length;
+		while (gap < len / 3){
+			gap = gap * 3 + 1; // <O(n^(3/2)) by Knuth,1973>: 1, 4, 13, 40, 121, ...
+		}
+		int tmp=0;
+		while(gap > 0){
+			for (i = gap; i < len; i++) {
+				for (j = i; j > 0; j -= gap){
+					if(j-gap>=00 && arr[j-gap] > arr[j]){
+						tmp = arr[j];
+						arr[j] = arr[j-1];
+						arr[j-1] = tmp;
+					}else{
+						break;
+					}
+				}
+			}
+			gap /= 3;
+		}
+		long t1 = System.currentTimeMillis();
+		System.out.println("shell耗时:"+(t1-t));
+	}
+	
 	public static void main(String[] args){
 		
 		int[] arrays = new int[1000000];
@@ -62,15 +122,21 @@ public class TestSort {
 			arrays[i] = rand.nextInt();
 		}
 		
-		bubble(arrays);
-//		for(int i=0;i<arrays.length;i++){
-//			System.out.println(arrays[i]);
-//		}
-		
+//		bubble(arrays);
 		
 //		select(arrays);
-//		for(int i=0;i<arrays.length;i++){
-//			System.out.println(arrays[i]);
-//		}
+		
+//		insert(arrays);
+//		sysout(arrays);
+		
+		shell(arrays);
+//		sysout(arrays);
+	}
+	
+	
+	private static void sysout(int[] arrays){
+		for(int i=0;i<arrays.length;i++){
+			System.out.println(arrays[i]);
+		}
 	}
 }
---
+++ src/com/elisonwell/algorithm/TestSort1.java
@@ -0,0 +1,119 @@
+package com.elisonwell.algorithm;
+
+import java.util.Random;
+
+/**
+ * 排序算法比较
+ * 此处实现均摘自维基百科
+ * @author duyisong
+ * @createAt 2016年6月15日
+ */
+public class TestSort1 {
+	
+	/**
+	 * 冒泡排序，与本人实现的相同
+	 * @param arr
+	 */
+	public static void bubble_sort(int[] arr) {
+		long t = System.currentTimeMillis();
+		int i, j, temp, len = arr.length;
+		for (i = 0; i < len - 1; i++)
+			for (j = 0; j < len - 1 - i; j++)
+				if (arr[j] > arr[j + 1]) {
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+		long t1 = System.currentTimeMillis();
+		System.out.println("bubble_sort耗时:"+(t1-t));
+	}
+	/**
+	 * 选择排序,比本人实现的性能略差
+	 * 数据量   耗时（s）
+	 * 100w    361
+	 * 10w     3
+	 * @param arr
+	 */
+	public static void select_sort(int[] arr) {
+		long t = System.currentTimeMillis();
+		int i, j, min, temp, len = arr.length;
+		for (i = 0; i < len - 1; i++) {
+			min = i;
+			for (j = i + 1; j < len; j++)
+				if (arr[min] > arr[j])
+					min = j;
+			temp = arr[min];
+			arr[min] = arr[i];
+			arr[i] = temp;
+		}
+		long t1 = System.currentTimeMillis();
+		System.out.println("select_sort耗时:"+(t1-t));
+	}
+	
+	/**
+	 * 插入排序，与我实现的算法性能差不多
+	 * 数据量   耗时（s）
+	 * 100w    387
+	 * 10w     4
+	 * @param arr
+	 */
+	public static void insert_sort(int[] arr){
+		long t = System.currentTimeMillis();
+		for (int i = 1; i < arr.length; i++ ) {
+			int temp = arr[i];
+			for (int j = i - 1; j >= 0 && arr[j] > temp; j-- ) {
+				arr[j + 1] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		long t1 = System.currentTimeMillis();
+		System.out.println("insert_sort耗时:"+(t1-t));
+	}
+	
+	/**
+	 * 希尔排序的另一种实现，该方法对于100w的数据耗时仅为226毫秒
+	 * @param arrays
+	 */
+	public static void shell_sort(int[] arr) {
+		long t = System.currentTimeMillis();
+		int gap = 1, i, j, len = arr.length;
+		int temp;
+		while (gap < len / 3)
+			gap = gap * 3 + 1; // <O(n^(3/2)) by Knuth,1973>: 1, 4, 13, 40, 121, ...
+		for (; gap > 0; gap /= 3)
+			for (i = gap; i < len; i++) {
+				temp = arr[i];
+				for (j = i - gap; j >= 0 && arr[j] > temp; j -= gap)
+					arr[j + gap] = arr[j];
+				arr[j + gap] = temp;
+		}
+		long t1 = System.currentTimeMillis();
+		System.out.println("shell_sort耗时:"+(t1-t));
+	}
+	
+	public static void main(String[] args){
+		
+		int[] arrays = new int[1000000];
+		Random rand = new Random();
+		for(int i=0;i<arrays.length;i++){
+			arrays[i] = rand.nextInt();
+		}
+		
+//		bubble_sort(arrays);
+		
+//		select_sort(arrays);
+		
+		insert_sort(arrays);
+//		sysout(arrays);
+		
+//		shell_sort(arrays);
+//		sysout(arrays);
+	}
+	
+	
+	private static void sysout(int[] arrays){
+		for(int i=0;i<arrays.length;i++){
+			System.out.println(arrays[i]);
+		}
+	}
+}
